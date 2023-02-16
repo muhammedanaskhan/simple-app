6512d2a07bf6945668187888533fa9da232057dd
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-02-15 22:18:22 -0800

    add and remove one by one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [activity, setActivity] = useState('')
+  const [listData, setListData] = useState([])
+
+  function addActivity() {
+    // setListData([...listData, activity])
+    setListData((listData) => {
+      const updatedList = [...listData, activity]
+      setActivity('')
+      return updatedList
+      
+    })
+  }
+
+  function removeActivity(i) {
+    const updated= listData.slice(i+1)
+    setListData(updated)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO App</h1>
+
+      <div>
+        <input type='text' className='activity__input' placeholder='Enter goal' value={activity} onChange={(e) => setActivity(e.target.value)}></input>
+        <button  onClick={addActivity}>Add</button>
+      </div>
+
+      <h2>Here're your targets</h2>
+
+      {listData!=[] && listData.map((entry, i) => {
+          return(
+            <>
+              <p key={i}>
+                <div>{entry}</div>
+                <div><button onClick={() => removeActivity(i)}>remove</button></div>
+              </p>
+            </>
+          )
+      }) }
+    
     </div>
   );
 }
